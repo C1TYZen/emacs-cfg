92410dc3d39bef64065e2d505b889aa6c9d408b3
Arseny Vasiliev <avasiliev@elvees.com>
2022-06-29 15:26:05 +0300

    improovment

--- init.el
+++ init.el
@@ -34,7 +34,6 @@
 (tool-bar-mode -1)
 (show-paren-mode t) ; включить выделение выражений между {},[],()
 (setq show-paren-style 'parenthesis) ; выделить цветом выражения между {},[],()
-(load-theme spacemacs-dark)
 
 ;;; Packages confguration
 (when (packages-installed-p)
@@ -75,6 +74,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
 (custom-set-faces
